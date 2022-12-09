a4478898fecf182dd2e8685ffe160969d9f98747
krJean <kaliregenold@gmail.com>
2022-12-08 19:47:34 -0500

    Complete 2021 day 7.

--- year_2021/solvers/day_7.py
+++ year_2021/solvers/day_7.py
@@ -1,19 +1,37 @@
 from typing import List
 from .common import get_input_path
 
-def read_input(input_type: str) -> List:
+def read_input(input_type: str) -> List[int]:
     input_path = get_input_path(__name__.split('_')[-1], input_type)
-    print(input_path)
     with open(input_path, 'r') as input_file:
-        input = []
+        input = [int(c) for c in input_file.readline().strip().split(',')]
     return input
 
-def part_1(input: List) -> int:
+def part_1(input: List[int]) -> int:
     ans = 0
+    least_fuel = float('inf')
+
+    for pos in range(max(input)+1):
+        curr_fuel = 0
+        for crab in input:
+            curr_fuel += abs(crab-pos)
+        least_fuel = min(least_fuel, curr_fuel)
+
+    ans = least_fuel
     return ans
 
-def part_2(input: List) -> int:
+def part_2(input: List[int]) -> int:
     ans = 0
+    least_fuel = float('inf')
+
+    for pos in range(max(input)+1):
+        curr_fuel = 0
+        for crab in input:
+            steps = abs(crab-pos)
+            curr_fuel += int((steps/2)*(steps+1))
+        least_fuel = min(least_fuel, curr_fuel)
+
+    ans = least_fuel
     return ans
 
 def solve(part: int, input_type: str) -> int:
