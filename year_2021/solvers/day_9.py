923a235e24a51211a18d17b1f8d9d6370082f674
krJean <kaliregenold@gmail.com>
2022-12-11 09:18:14 -0500

    Complete 2021 day 9 part 1.

--- year_2021/solvers/day_9.py
+++ year_2021/solvers/day_9.py
@@ -1,18 +1,35 @@
 from typing import List
 from .common import get_input_path
+import numpy as np
 
-def read_input(input_type: str) -> List:
+def read_input(input_type: str) -> List[List[int]]:
     input_path = get_input_path(__name__.split('_')[-1], input_type)
-    print(input_path)
     with open(input_path, 'r') as input_file:
-        input = []
+        input = [[int(c) for c in line.strip()] for line in input_file]
     return input
 
-def part_1(input: List) -> int:
+def part_1(input: List[List[int]]) -> int:
     ans = 0
+    total_risk_level = 0
+
+    # pad of max height + 1 around the grid so there are less conditionals
+    input = np.pad(input, [(1,1),(1,1)], constant_values=10)
+
+    for i in range(1,len(input)-1):
+        for j in range(1,len(input[0])-1):
+            curr_height = input[i][j]
+            adj_heights = [
+                input[i-1][j],
+                input[i+1][j],
+                input[i][j-1],
+                input[i][j+1]]
+            if min(adj_heights) > curr_height:
+                total_risk_level += curr_height+1
+
+    ans = total_risk_level
     return ans
 
-def part_2(input: List) -> int:
+def part_2(input: List[List[int]]) -> int:
     ans = 0
     return ans
 
